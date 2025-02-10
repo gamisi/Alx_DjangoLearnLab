ecd7ebe34b0e28d19f9e19472dbe51fd8bec00cf
gamisi <gamisi.ga@gmail.com>
2025-02-10 18:47:52 +0300

    added display,filters and search for BookAdmin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,8 +3,8 @@ from .models import Book
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_year')
-    search_fields = ('title', 'author', 'published_year')
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author', 'publication_year')
     list_filter = ('author', 'published_year')
 
 admin.site.register(Book, BookAdmin)
