e52787d6ee24da638ee1470ff06dbf7e572c64eb
Hurricane <gamisi.ga@gmail.com>
2025-02-22 20:40:15 +0300

    added relationship_app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,7 @@
 from .models import Author, Librarian, Library, Book
 
 Book.objects.get(author="ngugi wa thiongo")
-Library.objects.get(name="city").books.all()
+Library.objects.get(name="library_name").books.all()
 
 
 
