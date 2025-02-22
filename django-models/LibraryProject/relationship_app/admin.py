fb691049a4ea0bf5dfa6d393717f4be25f9574ba
Hurricane <gamisi.ga@gmail.com>
2025-02-22 19:45:11 +0300

    added relationship app

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Author, Librarian, Library, Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title","author")
+    search_fields =("title","author")
+
+class LibrarianAdmin(admin.ModelAdmin):
+    list_display = ("name","library")
+    search_fields =("name,library",)
+
+class LibraryAdmin(admin.ModelAdmin):
+    def books_list(self, obj):
+        return ", ".join([book.title for book in obj.books.all()])
+
+    list_display = ("name","books_list")
+    search_fields = ("name","books_list")
 
 # Register your models here.
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Librarian, LibrarianAdmin)
+admin.site.register(Library, LibraryAdmin)
+admin.site.register(Book, BookAdmin)
--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -7,19 +7,42 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-class Book(models.Model):
+"""class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    
+    def __str__(self):
+        title = self.title if self.title else 'Untitled'
+        author = self.author.name if self.author else 'Unknown Author'
+        return f"{title} by {author}"
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
-    books = models.ManyToManyField(Book, related_name="library")  
+    books = models.ManyToManyField(Book,related_name="library")  
+
+    def __str__(self):
+        self.name"""
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    
+    def __str__(self):
+        title = self.title if self.title else 'Untitled'
+        author = self.author.name if self.author else 'Unknown Author'
+        return f"{title} by {author}"
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book, related_name="libraries")  
+
+    def __str__(self):
+        return self.name if self.name else 'Unnamed Library' 
 
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")   
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarians")   
 
-    """
     def __str__(self):
-        return f"{self.title} by {self.author} in {self.published_year}"
-    """
+        return self.name
+    
