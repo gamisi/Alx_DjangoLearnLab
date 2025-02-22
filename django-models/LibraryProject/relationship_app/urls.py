085a37ea7fda37adb7009dc3611b1854c783e06e
Hurricane <gamisi.ga@gmail.com>
2025-02-23 00:48:46 +0300

    added DetailedView

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
+from .views import LibraryDetailView
 
 urlpatterns = [
 
     path("", views.index, name="index"),
     path('all_books/', views.book_list, name='all_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ import logging
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book, Librarian, Library, Author
+from django.views.generic import DetailView
 
 # Create your views here.
 
@@ -16,3 +17,13 @@ def book_list(request):
         print(f"Title: {book.title}, Author: {book.author.name}")
 
     return render(request, "relationship_app/list_books.html", context)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        return context
+    
