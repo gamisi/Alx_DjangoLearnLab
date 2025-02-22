8fd326fab28ba364b03b680c879bde6b4f0d609d
Hurricane <gamisi.ga@gmail.com>
2025-02-22 21:13:07 +0300

    new commits

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,7 +5,7 @@ Book.objects.get(author="ngugi wa thiongo")
 library_name = Library.objects.get(name="City")
 Library.objects.get(name=library_name).books.all()
 
-Author_name = Author.objects.get(name="Ngugi wa thiong'o")
+Author_name = Author.objects.get(name="Ngugi wa thiong'oo")
 Author = Author_name
 Author.objects.get(name=Author_name).books.filter(author = Author_name)
 
