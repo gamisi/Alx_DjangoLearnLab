8c5ad06dfc08c9e644d5b59ba208dfa306452e48
Hurricane <gamisi.ga@gmail.com>
2025-02-23 14:53:19 +0300

    added permissions

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,7 +8,9 @@ from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 #from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from .forms import BookForm
 
 # Create your views here.
