f0d5047ee3301e876e3a9307a59cdb4ee5f88b47
Hurricane <gamisi.ga@gmail.com>
2025-02-23 13:17:11 +0300

    updated class profile

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile (models.Model):
+    ROLE_CHOICES = [
+
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES, default='Member')
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    def __str__(self):
+        return self.role
+        
 class Author(models.Model):
     name = models.CharField(max_length=200)
 
@@ -46,18 +58,4 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 
-class UserProfile (models.Model):
 
-    ROLE_CHOICES = [
-
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    ]
-
-    role = models.CharField(max_length=50, choices=ROLE_CHOICES, default='Member')
-    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
-    
-
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
