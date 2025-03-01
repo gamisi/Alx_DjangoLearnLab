4cdd34f6fab1261ff24c09ae07e1edf6e2003853
Hurricane <gamisi.ga@gmail.com>
2025-03-01 14:00:54 +0300

    advanced models

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Book
+from .models import CustomUser
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
@@ -8,4 +9,5 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('author', 'published_year')
 
 admin.site.register(Book, BookAdmin)
-
+admin.site.register(CustomUser)
+#admin.site.register(CustomUserManager)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
 class Book(models.Model):
@@ -18,3 +19,23 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
     
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, date_of_birth, profile_photo=None, password=None, **extra_fields):
+        """
+        Create and return a regular user with an email, date_of_birth, and password.
+        """
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, date_of_birth=date_of_birth, profile_photo=profile_photo, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, date_of_birth, profile_photo=None, password=None, **extra_fields):
+        """
+        Create and return a superuser with an email, date_of_birth, and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, date_of_birth, profile_photo, password, **extra_fields)
