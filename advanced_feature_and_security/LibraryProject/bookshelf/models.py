1aa75312ebda5ff476d3b32deb9d1bc0832a0658
Hurricane <gamisi.ga@gmail.com>
2025-03-01 13:39:18 +0300

    added advanced features

--- advanced_feature_and_security/LibraryProject/bookshelf/models.py
+++ advanced_feature_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 class Book(models.Model):
@@ -9,4 +10,11 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.title} by {self.author} in {self.published_year}"
 
+    
+class CustomUser(AbstractBaseUser):
+    date_of_birth = date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
     
