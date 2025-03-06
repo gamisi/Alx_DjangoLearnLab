01894ffbec45810b7ae57310480194be48eab31e
Hurricane <gamisi.ga@gmail.com>
2025-03-06 14:19:42 +0300

    added api routes

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets, filters
+from .serializers import BookSerializer
+from .models import Book
 
 # Create your views here.
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
