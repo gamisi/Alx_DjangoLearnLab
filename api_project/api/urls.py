367b0a3d8e92293a04da09feb1d2a276bd7c13dc
Hurricane <gamisi.ga@gmail.com>
2025-03-06 14:27:25 +0300

    added api routes

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,6 +3,7 @@ from .views import BookList
 
 urlpatterns = [
 
-    path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
-    
+    path('books/', BookList.as_view(), name='book-list'),  
+    path('',  include('api.urls')),
+
 ]
