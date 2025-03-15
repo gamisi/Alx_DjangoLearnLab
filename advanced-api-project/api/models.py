326c5f3628d7ad5a4d9da5cb9bfc74c5f4a8254e
gamisi <gamisi.ga@gmail.com>
2025-03-16 01:30:40 +0300

    added models Book and Author

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -12,7 +12,7 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    publibcation_year = models.IntegerField(max_length=100)
+    publication_year = models.IntegerField()
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
 
     def __str__(self):
