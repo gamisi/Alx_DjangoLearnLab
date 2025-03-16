4e9fc64b76ef093078eaf1f181b04a6e1a88c49c
gamisi <gamisi.ga@gmail.com>
2025-03-16 11:53:08 +0300

    added url parameters in the api/urls.py file

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,10 +5,10 @@ from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView,
 
 urlpatterns = [
 
-    path('books/', BookListView.as_view(), name='book-list'),  # List and create books
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # View a specific book by ID
-    path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create a new book
-    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),  # Update a book
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+    path('books/', BookListView.as_view(), name='book_list'),  # List and create books
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),  # View a specific book by ID
+    path('books/create/', BookCreateView.as_view(), name='book_create'),  # Create a new book
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book_update'),  # Update a book
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book_delete'),  # Delete a book
 
 ]
