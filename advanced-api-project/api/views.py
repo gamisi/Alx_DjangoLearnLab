a7d71eab926df48542f39d762f931be3a473f765
gamisi <gamisi.ga@gmail.com>
2025-03-16 11:55:10 +0300

    update permissions for custom views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 # List all Books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = AllowAny
-    
+    permission_classes = IsAuthenticatedOrReadOnly
+
 # retrieve a specific book based on the id
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = AllowAny
+    permission_classes = IsAuthenticatedOrReadOnly
 
 # create a book
 class BookCreateView(generics.CreateAPIView):
