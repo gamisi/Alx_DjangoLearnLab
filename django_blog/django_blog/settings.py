00e837a43293ca819fa9a5cbf02a8f6a640c8247
gamisi <gamisi.ga@gmail.com>
2025-03-18 13:31:09 +0300

    added Database Credentials

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
+
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',
+        'PASSWORD':'',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
