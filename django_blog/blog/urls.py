6825c0b17456137d4234c7d19f040b4a5146bf6b
gamisi <gamisi.ga@gmail.com>
2025-03-18 21:34:29 +0300

    changes to view

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     # Authentication URLs
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
-    
+
     # Registration and profile
     path('register/', register, name='register'),
     path('profile/', profile, name='profile'),
+    
 ]
