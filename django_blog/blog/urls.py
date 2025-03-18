2c2414760b6d32c04228cc699c9f2c1652d985d2
gamisi <gamisi.ga@gmail.com>
2025-03-19 01:20:58 +0300

    fixed url bugs

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,8 +18,8 @@ urlpatterns = [
 
     path('posts/', views.PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('add_post/', views.PostCreateView.as_view(), name='post_create'),
-    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete')
         
 ]
