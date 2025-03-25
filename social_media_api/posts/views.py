7174ce5851fe28ebe57a405397925dce72e78570
gamisi <gamisi.ga@gmail.com>
2025-03-25 22:28:14 +0300

    added urls

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from serializers import PostSerializer, CommentSerializer
+from .serializers import PostSerializer, CommentSerializer
 from rest_framework import generics
 from .models import Post, Comment
 
