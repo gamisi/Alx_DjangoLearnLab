3d6b5431b7e75b2032c99f75f380a14cae0ecc70
gamisi <gamisi.ga@gmail.com>
2025-03-26 21:18:22 +0300

    updated settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -107,9 +107,15 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
+        
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': '',
+        'PORT': '',
+        'USERNAME': '',
+        'PASSWORD': ''
     }
 }
 
